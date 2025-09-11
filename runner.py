eefd6c6059edc556a72b6e9863ba41b28548c238
Jonas Nordqvist <jonas.nordqvist@lnu.se>
2025-09-11 17:07:07 +0200

    new simul

--- runner.py
+++ runner.py
@@ -1,4 +1,5 @@
 from experiments_src.simulations import train_run
+from experiments_src.simulations_trada import train_run_trada
 import argparse
 
 #v1_list = [0.005, 0.007, 0.01, 0.03, 0.07]
@@ -11,5 +12,14 @@ if __name__ == "__main__":
         default="0",
         help="idx of val of v1",
     )
+    parser.add_argument(
+        "--ver",
+        dest="v2",
+        default="sim",
+        help="idx of val of v1",
+    )
     args = parser.parse_args()
-    train_run(args.v1)
+    if args.v2 == 'sim':
+        train_run(args.v1)
+    else:
+        train_run_trada(args.v1)
