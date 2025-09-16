086687bd5c9e045c3509d7263442d1bbb46494ee
Jonas Nordqvist <jonas.nordqvist@lnu.se>
2025-09-16 23:55:54 +0200

    latest

--- run_script_tester.sh
+++ run_script_tester.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {0..4}
+for i in {2..4}
 do
   # Create a new screen session named "run_session_i" and execute run.py with --arg set to the current value of i
   screen -dmS "rs_run_$i" bash -c "python runner.py --v1 $i --ver 'rs'"
