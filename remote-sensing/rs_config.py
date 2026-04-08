2c844b92d24a0f78037628119fabd8885ac74d3e
DageBjorne <dag.bjornberg@softwerk.se>
2026-04-08 10:51:52 +0200

    new trada res

--- remote-sensing/rs_config.py
+++ remote-sensing/rs_config.py
@@ -31,13 +31,15 @@ target_tree_size_list = [1, 2, 3, 4, 5, 6, 7]
 param_grid_XGBoost = list(itertools.product(v_list, target_tree_size_list))
 
 #Two-stage TrAdaBoost.R2 configs
-lr_list = [0.05, 0.1, 0.25, 0.5, 1.0]
+lr_list = [0.1, 0.5, 1.0]
 n_estimators_list = [10, 20, 30, 50, 100, 150]
+tree_size_list = [1,2,3,4]
 # n_estimators_fs_list = [30]
 # cv_list = [10]
 
 param_grid_TwoTrada = list(itertools.product(lr_list, 
-                                             n_estimators_list))
+                                             n_estimators_list,
+                                             tree_size_list))
 
 #laser metrics to use
 predictor_columns = [
